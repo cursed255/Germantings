785002a3e95be07af45900ce07fc0ea021ab6d04
cursed255 <bradley.potter@live.co.uk>
2015-04-30 17:49:14 +0100

    insert user

--- src/main/java/DatabaseManager.java
+++ src/main/java/DatabaseManager.java
@@ -1,3 +1,10 @@
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,4 +17,30 @@
  */
 public class DatabaseManager {
     
+    
+    String a = "";
+    String b = "";
+    String c = "";
+    String d = "";
+    
+    public void addstudent() throws IOException, ClassNotFoundException, SQLException {
+        System.out.println("add Users");
+        SimpleDataSource.init("database.properties");
+        Connection conn = SimpleDataSource.getConnection();
+        Statement st = conn.createStatement();
+
+        try {
+            String command = "INSERT INTO User(user_id, user_name, password, user_level)" + "VALUES (?,?,?,?)";
+            PreparedStatement preperedStatement = conn.prepareStatement(command);
+            preperedStatement.setString(1, a);
+            preperedStatement.setString(2, b);
+            preperedStatement.setString(3, c);
+            preperedStatement.setString(3, c);
+            preperedStatement.executeUpdate();
+
+        } finally {
+
+            conn.close();
+        }
+    }
 }
